a4b78c993a08c464cd769c92b49ce430e2e4b711
wheatgreaser <wheatgreaser3000@gmail.com>
2025-02-20 21:04:43 +0530

    yo

--- movement.gd
+++ movement.gd
@@ -20,8 +20,10 @@ func _unhandled_input(event: InputEvent) -> void:
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
 			neck.rotate_y(-event.relative.x * 0.01)
+			
 			rotation.y = -event.relative.y * 0.01
 			camera.rotate_x(-event.relative.y * 0.01)
+			rotate_x(-event.relative.y * 0.01)
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
 
 func _ready():
